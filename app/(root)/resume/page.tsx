e1673861165ffe33af91bbfd811318f868380d51
manannayak19 <nayakmanan1911@gmail.com>
2025-12-08 22:19:48 -0500

    added a default export to resume page so deployment works

--- app/(root)/resume/page.tsx
+++ app/(root)/resume/page.tsx
@@ -1,23 +1,33 @@
-import { Bitcount_Grid_Single } from "next/font/google";
-
-const btc = Bitcount_Grid_Single({
-  weight: "500",
-});
-
-export default function Page() {
+export default function ResumePage() {
   return (
-    <div className="mt-20 flex justify-center">
-      <div className=" flex flex-col items-center bg-[#EFF8E2] p-4 border rounded-2xl md:w-5xl">
-        <div className={`text-3xl md:text-6xl ${btc.className} `}>
-          manan nayak
-        </div>
-        <div className="text-lg pt-3 text-center">
-          full stack developer with MERN stack experience, seeking to build
-          scalable web apps and grow expertise in JavaScript technologies.
-          actively exploring opportunities to learn and build!
-        </div>
-        <div className={`text-xl md:text-4xl ${btc.className} `}>education</div>
-      </div>
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-3xl font-bold">Resume</h1>
+      <p className="mt-4">building a cool resume</p>
     </div>
   );
 }
+
+
+// import { Bitcount_Grid_Single } from "next/font/google";
+
+// const btc = Bitcount_Grid_Single({
+//   weight: "500",
+// });
+
+// export default function Page() {
+//   return (
+//     <div className="mt-20 flex justify-center">
+//       <div className=" flex flex-col items-center bg-[#EFF8E2] p-4 border rounded-2xl md:w-5xl">
+//         <div className={`text-3xl md:text-6xl ${btc.className} `}>
+//           manan nayak
+//         </div>
+//         <div className="text-lg pt-3 text-center">
+//           full stack developer with MERN stack experience, seeking to build
+//           scalable web apps and grow expertise in JavaScript technologies.
+//           actively exploring opportunities to learn and build!
+//         </div>
+//         <div className={`text-xl md:text-4xl ${btc.className} `}>education</div>
+//       </div>
+//     </div>
+//   );
+// }
